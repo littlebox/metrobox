0f91c069d3df525f73db4b4001b6174e3337370d
Nicolás Pennesi <nicolaspennesi@gmail.com>
2014-11-26 21:09:33 -0300

    Change default salt and cipherSeed of CakePHP

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'hcubUrENEM484CPqd4wFrNbALxBGuhKCQ6yMxGVG');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '26560408962065020915623093606');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
