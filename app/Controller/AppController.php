839c335b3c1b57b89f276ef25e24504af9b7d0d2
lucianoar <luciano@littlebox.com.ar>
2014-11-27 20:21:24 -0300

    Authentication Users

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,4 +31,29 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+	public $components = array(
+        'Session',
+        'Auth' => array(
+            'loginRedirect' => array(
+                'controller' => 'pages',
+                'action' => 'display'
+            ),
+            'logoutRedirect' => array(
+                'controller' => 'pages',
+                'action' => 'display',
+                'home'
+            ),
+            'authenticate' => array(
+                'Form' => array(
+                    'passwordHasher' => 'Blowfish',
+                    'fields' => array('username' => 'email', 'password' => 'password'),
+                )
+            )
+        )
+    );
+
+    public function beforeFilter() {
+        $this->Auth->allow('index', 'view');
+    }
+    //...
 }
