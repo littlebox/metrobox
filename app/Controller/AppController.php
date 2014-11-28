54571f9ca31d315a4b137930a54028c010cd672e
lucianoar <luciano@littlebox.com.ar>
2014-11-28 18:03:56 -0300

    Install DebugKit plugin

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -32,6 +32,7 @@ App::uses('Controller', 'Controller');
  */
 class AppController extends Controller {
 	public $components = array(
+        'DebugKit.Toolbar',
         'Session',
         'Auth' => array(
             'loginRedirect' => array(
