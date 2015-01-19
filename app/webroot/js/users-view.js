95f445ef3d00d8bdd46909805a1c2030eef3df43
lucianoar <luciano@littlebox.com.ar>
2015-01-19 18:22:22 -0300

    Fix math random function

--- app/webroot/js/users-view.js
+++ app/webroot/js/users-view.js
@@ -70,10 +70,10 @@ var UsersView = {
 			var thumbnail = $('.fileinput-new.thumbnail img');
 			
 			var src = profileUserpic.attr('src');
-			profileUserpic.attr('src', '').attr('src', src +'?'+ Math.random); //download new image without cache
+			profileUserpic.attr('src', '').attr('src', src +'?'+ Math.random()); //download new image without cache
 
 			var src2 = thumbnail.attr('src');
-			thumbnail.attr('src', '').attr('src', src2 +'?'+ Math.random); //download new image without cache
+			thumbnail.attr('src', '').attr('src', src2 +'?'+ Math.random()); //download new image without cache
 
 		}
 
