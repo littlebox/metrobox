974bbad1cee96b0913c33ffb0545b1b9c89eb66f
lucianoar <luciano@littlebox.com.ar>
2015-07-16 10:31:34 -0300

    CORS in wineries/get

--- app/Controller/WineriesController.php
+++ app/Controller/WineriesController.php
@@ -45,7 +45,12 @@ class WineriesController extends AppController {
 	}
 
 	public function get() {
-		$this->request->allowMethod('ajax'); //Only Ajax
+		// $this->request->allowMethod('ajax'); //Only Ajax
+		// $this->Security->csrfCheck = false;
+		header('Access-Control-Allow-Origin:*');
+		header('Access-Control-Allow-Methods:*');
+		header('Access-Control-Allow-Headers:X-Requested-With');
+		header('X-pennesi-puto?:si!!!');
 
 		//Render always as json
 		$this->RequestHandler->renderAs($this, 'json');
