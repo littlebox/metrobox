3569eda72f7c9d847f1398a2a921d4a454368753
Nicolás Pennesi <nicolaspennesi@gmail.com>
2015-07-16 11:37:45 -0300

    Add group by winery id in get wineries

--- app/Controller/WineriesController.php
+++ app/Controller/WineriesController.php
@@ -47,7 +47,6 @@ class WineriesController extends AppController {
 		header('Access-Control-Allow-Origin:*');
 		header('Access-Control-Allow-Methods:*');
 		header('Access-Control-Allow-Headers:X-Requested-With');
-		header('X-pennesi-puto?:si!!!');
 
 		//Render always as json
 		$this->RequestHandler->renderAs($this, 'json');
@@ -142,6 +141,7 @@ class WineriesController extends AppController {
 					)
 				),
 			),
+			'group' => 'Winery.id',
 			'order' => array(
 				'Winery.priority DESC'
 			),
