d74142a7f35122229390459cb00135aa97c73c7e
lucianoar <luciano@littlebox.com.ar>
2015-09-23 19:31:22 -0300

    Test mp notifications

--- app/Controller/ReservesController.php
+++ app/Controller/ReservesController.php
@@ -256,9 +256,36 @@ class ReservesController extends AppController {
 		))
 		$mp = new MP('8915881018899740', 'VFVdIwFOZQLabpCDnN6AvgbTzVT2mqju');
 
-		$payment_info = $mp->get_payment_info($_GET["id"]);
-		if ($payment_info["status"] == 200) {
-			file_put_contents(APP.'mp_notification.txt',json_encode($payment_info));
+		if($_GET["topic"] == 'payment'){
+			$payment_info = $mp->get("/collections/notifications/" . $_GET["id"]);
+			$merchant_order_info = $mp->get("/merchant_orders/" . $payment_info["response"]["collection"]["merchant_order_id"]);
+		// Get the merchant_order reported by the IPN.
+		} else if($_GET["topic"] == 'merchant_order'){
+			$merchant_order_info = $mp->get("/merchant_orders/" . $_GET["id"]);
+		}
+		file_put_contents(APP.'/mp_notifications.txt', json_encode($merchant_order_info));
+
+		if ($merchant_order_info["status"] == 200) {
+			// If the payment's transaction amount is equal (or bigger) than the merchant_order's amount you can release your items
+			/*$paid_amount = 0;
+
+			foreach ($merchant_order_info["response"]["payments"] as $payment) {
+				if ($payment['status'] == 'approved'){
+					$paid_amount += $payment['transaction_amount'];
+				}
+			}
+
+			if($paid_amount >= $merchant_order_info["response"]["total_amount"]){
+				if(count($merchant_order_info["response"]["shipments"]) > 0) { // The merchant_order has shipments
+					if($merchant_order_info["response"]["shipments"][0]["status"] == "ready_to_ship"){
+						print_r("Totally paid. Print the label and release your item.");
+					}
+				} else { // The merchant_order don't has any shipments
+					print_r("Totally paid. Release your item.");
+				}
+			} else {
+				print_r("Not paid yet. Do not release your item.");
+			}*/
 		}
 	}
 
