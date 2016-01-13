2a93da6f53794bd01d123c9b39afed6344226102
Nicolás Pennesi <nicolaspennesi@gmail.com>
2016-01-13 13:48:56 -0300

    Start with PayPal notifications

--- app/Controller/ReservesController.php
+++ app/Controller/ReservesController.php
@@ -20,7 +20,7 @@ class ReservesController extends AppController {
 
 	public function beforeFilter() {
 		parent::beforeFilter();
-		$this->Auth->allow('api_add','mp_notification', 'cancel');
+		$this->Auth->allow('api_add','mp_notification', 'pp_notification', 'cancel');
 	}
 
 
@@ -252,8 +252,8 @@ class ReservesController extends AppController {
 
 			$apiContext = new \PayPal\Rest\ApiContext(
 				new \PayPal\Auth\OAuthTokenCredential(
-					'Aej6N4FTTOW0jNaK_S23ipWPrSdgyluZL09oVpGuYl0VbAm5wk8ypY5h76VHv9R8kcWQ0aLnwJDOLX1h',     // ClientID
-					'EELXHrny_38jlDZqwd78pZqA6Lsge85bqkh7N5uM_hd0M1xeMZcr0NcJqJBSEKln_JDY6_jI64nNqd74'      // ClientSecret
+					'Aej6N4FTTOW0jNaK_S23ipWPrSdgyluZL09oVpGuYl0VbAm5wk8ypY5h76VHv9R8kcWQ0aLnwJDOLX1h',	// ClientID
+					'EELXHrny_38jlDZqwd78pZqA6Lsge85bqkh7N5uM_hd0M1xeMZcr0NcJqJBSEKln_JDY6_jI64nNqd74'	// ClientSecret
 				)
 			);
 
@@ -378,31 +378,30 @@ class ReservesController extends AppController {
 
 			$amount = new Amount();
 			$amount->setCurrency("USD")
-					->setTotal($totalPrice*$currencyRate);
-					// ->setDetails($details);
+				->setTotal($totalPrice*$currencyRate);
+				// ->setDetails($details);
 
 			$transaction = new Transaction();
 			$transaction->setAmount($amount)
-					->setItemList($itemList)
-					->setDescription("Payment description")
-					->setInvoiceNumber(uniqid());
+				->setItemList($itemList)
+				->setDescription("Reserva Wineobs")
+				->setInvoiceNumber($invoice['Invoice']['id']);
 
-			$baseUrl = 'http://www.wineobs.com/'; //getBaseUrl();
 			$redirectUrls = new RedirectUrls();
-			$redirectUrls->setReturnUrl("$baseUrl/payment_success")
-				->setCancelUrl("$baseUrl/payment_failure");
+			$redirectUrls->setReturnUrl("http://wineobs.com/payment_success")
+				->setCancelUrl("http://wineobs.com/payment_failure");
 
 
 			$payment = new Payment();
 			$payment->setIntent("sale")
-			    ->setPayer($payer)
-			    ->setRedirectUrls($redirectUrls)
-			    ->setTransactions(array($transaction));
+				->setPayer($payer)
+				->setRedirectUrls($redirectUrls)
+				->setTransactions(array($transaction));
 
 			$request = clone $payment;
 
 			try {
-			    $payment->create($apiContext);
+				$payment->create($apiContext);
 			} catch (Exception $e) {
 					$data['error'] = $e;
 			}
@@ -620,6 +619,122 @@ class ReservesController extends AppController {
 		$clientEmail->send();
 
 		echo 'ok';
+	}
+
+	public function pp_notification(){
+
+		// CONFIG: Enable debug mode. This means we'll log requests into 'ipn.log' in the same directory.
+		// Especially useful if you encounter network errors or other intermittent problems with IPN (validation).
+		// Set this to 0 once you go live or don't require logging.
+		define("DEBUG", 1);
+		// Set to 0 once you're ready to go live
+		define("USE_SANDBOX", 1);
+		define("LOG_FILE", "./ipn.log");
+		// Read POST data
+		// reading posted data directly from $_POST causes serialization
+		// issues with array data in POST. Reading raw POST data from input stream instead.
+		$raw_post_data = file_get_contents('php://input');
+		$raw_post_array = explode('&', $raw_post_data);
+		$myPost = array();
+		foreach ($raw_post_array as $keyval) {
+			$keyval = explode ('=', $keyval);
+			if (count($keyval) == 2)
+				$myPost[$keyval[0]] = urldecode($keyval[1]);
+		}
+		// read the post from PayPal system and add 'cmd'
+		$req = 'cmd=_notify-validate';
+		if(function_exists('get_magic_quotes_gpc')) {
+			$get_magic_quotes_exists = true;
+		}
+		foreach ($myPost as $key => $value) {
+			if($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
+				$value = urlencode(stripslashes($value));
+			} else {
+				$value = urlencode($value);
+			}
+			$req .= "&$key=$value";
+		}
+		// Post IPN data back to PayPal to validate the IPN data is genuine
+		// Without this step anyone can fake IPN data
+		if(USE_SANDBOX == true) {
+			$paypal_url = "https://www.sandbox.paypal.com/cgi-bin/webscr";
+		} else {
+			$paypal_url = "https://www.paypal.com/cgi-bin/webscr";
+		}
+		$ch = curl_init($paypal_url);
+		if ($ch == FALSE) {
+			return FALSE;
+		}
+		curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+		curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+		if(DEBUG == true) {
+			curl_setopt($ch, CURLOPT_HEADER, 1);
+			curl_setopt($ch, CURLINFO_HEADER_OUT, 1);
+		}
+		// CONFIG: Optional proxy configuration
+		//curl_setopt($ch, CURLOPT_PROXY, $proxy);
+		//curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
+		// Set TCP timeout to 30 seconds
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: Close'));
+		// CONFIG: Please download 'cacert.pem' from "http://curl.haxx.se/docs/caextract.html" and set the directory path
+		// of the certificate as shown below. Ensure the file is readable by the webserver.
+		// This is mandatory for some environments.
+		//$cert = __DIR__ . "./cacert.pem";
+		//curl_setopt($ch, CURLOPT_CAINFO, $cert);
+		$res = curl_exec($ch);
+		if (curl_errno($ch) != 0) // cURL error
+			{
+			if(DEBUG == true) {
+				error_log(date('[Y-m-d H:i e] '). "Can't connect to PayPal to validate IPN message: " . curl_error($ch) . PHP_EOL, 3, LOG_FILE);
+			}
+			curl_close($ch);
+			exit;
+		} else {
+				// Log the entire HTTP response if debug is switched on.
+				if(DEBUG == true) {
+					error_log(date('[Y-m-d H:i e] '). "HTTP request of validation request:". curl_getinfo($ch, CURLINFO_HEADER_OUT) ." for IPN payload: $req" . PHP_EOL, 3, LOG_FILE);
+					error_log(date('[Y-m-d H:i e] '). "HTTP response of validation request: $res" . PHP_EOL, 3, LOG_FILE);
+				}
+				curl_close($ch);
+		}
+		// Inspect IPN validation result and act accordingly
+		// Split response headers and payload, a better way for strcmp
+		$tokens = explode("\r\n\r\n", trim($res));
+		$res = trim(end($tokens));
+		if (strcmp ($res, "VERIFIED") == 0) {
+			debug($_POST);die();
+			// check whether the payment_status is Completed
+			// check that txn_id has not been previously processed
+			// check that receiver_email is your PayPal email
+			// check that payment_amount/payment_currency are correct
+			// process payment and mark item as paid.
+			// assign posted variables to local variables
+			//$item_name = $_POST['item_name'];
+			//$item_number = $_POST['item_number'];
+			//$payment_status = $_POST['payment_status'];
+			//$payment_amount = $_POST['mc_gross'];
+			//$payment_currency = $_POST['mc_currency'];
+			//$txn_id = $_POST['txn_id'];
+			//$receiver_email = $_POST['receiver_email'];
+			//$payer_email = $_POST['payer_email'];
+
+			if(DEBUG == true) {
+				error_log(date('[Y-m-d H:i e] '). "Verified IPN: $req ". PHP_EOL, 3, LOG_FILE);
+			}
+		} else if (strcmp ($res, "INVALID") == 0) {
+			// log for manual investigation
+			// Add business logic here which deals with invalid IPN messages
+			if(DEBUG == true) {
+				error_log(date('[Y-m-d H:i e] '). "Invalid IPN: $req" . PHP_EOL, 3, LOG_FILE);
+			}
+		}
+
 	}
 
 	public function edit() {
